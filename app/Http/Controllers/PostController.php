a0fb31dbf8c95d31bb63ebdd5675e58f052c53f1
Daniel Khan <samir.khan.w@gmail.com>
2017-11-30 13:13:18 +0200

    HomeWork v1.1 (Pagination added)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('index', ['posts' => Post::orderBy('id', 'DESC')->get()]);
+        return view('index', ['posts' => Post::orderBy('id', 'DESC')->paginate(20)]);
     }
 
     /**
